b25b767af1720e8c5607180e97bcfa3b57e0a569
weltloose <982899377@qq.com>
2019-07-25 03:24:17 +0800

    second_version

--- server.c
+++ server.c
@@ -7,12 +7,29 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <stdlib.h>
+#include <signal.h>
+#include <sys/wait.h>
 
 #define MSERV_PORT 9878
 #define MSERV_IP "172.26.67.159"
 #define TEXT_MAX_SIZE 100
 
 typedef struct sockaddr SA;
+
+void sigchld_handler(int signo){
+	int chld_pidt;
+	while((chld_pidt = waitpid(-1, NULL, WNOHANG)) > 0){
+		printf("%d child process terminated.\n", chld_pidt);
+	}
+}
+
+void Signal(int signo, void (*func)(int)){
+	struct sigaction newsig;
+	newsig.sa_handler = func;
+	sigemptyset(&newsig.sa_mask);
+	newsig.sa_flags = 0;
+	sigaction(signo, &newsig, NULL);
+}
 
 void handle(int socket){
 	char temp[TEXT_MAX_SIZE];
@@ -24,6 +41,8 @@ again:
 	if(read_n < 0) goto again;
 	if(read_n == 0) return;
 }
+
+
 
 int main(){
 	int sockfd_listen = socket(AF_INET, SOCK_STREAM, 0);
@@ -41,6 +60,8 @@ int main(){
 		fprintf(stderr, "Can't listen.\n");
 		exit(0);
 	}
+	Signal(SIGCHLD, sigchld_handler);
+
 	socklen_t client_size = sizeof(client);
 	while(1){
 		sockfd_connect = accept(sockfd_listen, (SA *)&client, &client_size);
